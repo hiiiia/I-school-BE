03a4121eb5b48cdef20d8b4b893a4f45df08da69
hiiiia <90683622+hiiiia@users.noreply.github.com>
2024-07-27 16:48:39 +0900

    Feature/#24

--- domain/timetable/timetable_router.py
+++ domain/timetable/timetable_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
 
 from database import get_db
@@ -14,3 +14,25 @@ router = APIRouter(
 def get_course_timetables(timetable_id: int, db: Session = Depends(get_db)):
     course_timetables = timetable_crud.read_course_timetable(timetable_id, db)
     return Response(success=True, data=course_timetables, error=None)
+
+
+@router.post("/timetables/{timetable_id}", response_model=Response)
+def post_course_to_timetable(
+        timetable_id: int,
+        userId: int = Body(..., embed=True),
+        code: str = Body(..., embed=True),
+        db: Session = Depends(get_db)):
+
+    timetable_crud.insert_course_to_timetable(timetable_id, userId, code, db)
+    return Response(success=True, data=None, error=None)
+
+
+@router.delete("/timetables/{timetable_id}", response_model=Response)
+def del_course_from_timetable(
+        timetable_id: int,
+        userId: int = Body(..., embed=True),
+        code: str = Body(..., embed=True),
+        db: Session = Depends(get_db)):
+
+    timetable_crud.delete_course_from_timetable(timetable_id, userId, code, db)
+    return Response(success=True, data=None, error=None)
